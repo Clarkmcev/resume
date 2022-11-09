947671a8936d4c238b8707d571c4a50e5e400d52
Clark68500 <mcevoyclark@gmail.com>
2022-11-09 08:44:42 +0100

    responsiveness

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,8 +17,8 @@ function HomePage() {
       <>
         <div className="content">
             <motion.p className="text-third" variants={boxVariant} initial="hidden" animate="visible" transition={{delay:0.90}}>Greetings passenger, my name is</motion.p>
-            <motion.div className="text-8xl font-bold"  variants={boxVariant} initial="hidden" animate="visible" transition={{delay:1.05}}>Clark McEvoy<span className="text-third">.</span></motion.div>
-            <motion.div className="text-8xl font-light text-third" variants={boxVariant} initial="hidden" animate="visible" transition={{delay:1.20}}>Frontend Developer<span className="text-third"></span></motion.div>
+            <motion.div className="text-5xl sm:text-8xl font-bold"  variants={boxVariant} initial="hidden" animate="visible" transition={{delay:1.05}}>Clark McEvoy<span className="text-third">.</span></motion.div>
+            <motion.div className="text-6xl lg:text-8xl font-light text-third" variants={boxVariant} initial="hidden" animate="visible" transition={{delay:1.20}}>Frontend Developer<span className="text-third"></span></motion.div>
             <div className="paragraph">
               <motion.div variants={boxVariant} initial="hidden" animate="visible" transition={{delay:1.45}}>I build solid and scalable products with great user experiences.</motion.div>
               <motion.div variants={boxVariant} initial="hidden" animate="visible" transition={{delay:1.60}}>Software engineer from Colmar, France.</motion.div>
